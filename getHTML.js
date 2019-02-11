ebda20cdc0a656816afcc4c748be0a30f8fa1661
Darren Picard <darrenpicard25@gmail.com>
2019-02-11 19:49:09 +0000

    created file getHTML.js that takes in the option and a callback function to do something with the data once its read

---
+++ getHTML.js
@@ -0,0 +1,27 @@
+const https = require('https');
+
+function getHTML (options, callback) {
+
+  /* Add your code here */
+  https.get(options, function (response) {
+    let fullFile = '';
+    response.on('data', function (dataChunk) {
+      fullFile += dataChunk;
+    });
+    response.on('end', function () {
+      printHTML(fullFile);
+    });
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
